41937d5fd929e60ef8edf2a9985038ecf6d315e4
Bobby Wang <secure@bofeiwang.com>
2020-12-04 16:27:59 +1100

    add initial example

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Initial version of minimum-window-size library
+
+# Run example locally
+```shell script
+npm i
+cd example
+yarn install
+yarn start
+```
---
+++ example/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import MinimumWindowSize from "minimum-window-size";
+
+const App = () => {
+  return <MinimumWindowSize width={768} height={768}>test</MinimumWindowSize>
+}
+
+export default App
